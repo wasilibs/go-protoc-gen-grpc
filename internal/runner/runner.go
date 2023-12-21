183d7668f87306351ed085a9c46e56132b73bae5
Anuraag Agrawal <anuraaga@gmail.com>
2023-12-21 14:11:38 +0900

    Add release workflow

--- internal/runner/runner.go
+++ internal/runner/runner.go
@@ -3,7 +3,6 @@ package runner
 import (
 	"context"
 	"crypto/rand"
-	"io/fs"
 	"log"
 	"os"
 	"strings"
@@ -12,8 +11,6 @@ import (
 	wazero "github.com/wasilibs/wazerox"
 	"github.com/wasilibs/wazerox/api"
 	"github.com/wasilibs/wazerox/experimental"
-	"github.com/wasilibs/wazerox/experimental/sys"
-	"github.com/wasilibs/wazerox/experimental/sysfs"
 	"github.com/wasilibs/wazerox/imports/wasi_snapshot_preview1"
 	wzsys "github.com/wasilibs/wazerox/sys"
 )
@@ -29,9 +26,6 @@ func Run(name string, wasm []byte) {
 	args := []string{name}
 	args = append(args, os.Args[1:]...)
 
-	fsCfg := wazero.NewFSConfig().(sysfs.FSConfig).WithSysFSMount(cmdFS{cwd: sysfs.DirFS("."), root: sysfs.DirFS("/")}, "/")
-	fsCfg = fsCfg.(sysfs.FSConfig).WithRawPaths()
-
 	cfg := wazero.NewModuleConfig().
 		WithSysNanosleep().
 		WithSysNanotime().
@@ -40,8 +34,7 @@ func Run(name string, wasm []byte) {
 		WithStdout(os.Stdout).
 		WithStdin(os.Stdin).
 		WithRandSource(rand.Reader).
-		WithArgs(args...).
-		WithFSConfig(fsCfg)
+		WithArgs(args...)
 	for _, env := range os.Environ() {
 		k, v, _ := strings.Cut(env, "=")
 		cfg = cfg.WithEnv(k, v)
@@ -54,64 +47,4 @@ func Run(name string, wasm []byte) {
 		}
 		log.Fatal(err)
 	}
-}
-
-type cmdFS struct {
-	cwd  sys.FS
-	root sys.FS
-}
-
-func (fs cmdFS) OpenFile(path string, flag sys.Oflag, perm fs.FileMode) (sys.File, sys.Errno) {
-	return fs.fs(path).OpenFile(path, flag, perm)
-}
-
-func (fs cmdFS) Lstat(path string) (wzsys.Stat_t, sys.Errno) {
-	return fs.fs(path).Lstat(path)
-}
-
-func (fs cmdFS) Stat(path string) (wzsys.Stat_t, sys.Errno) {
-	return fs.fs(path).Stat(path)
-}
-
-func (fs cmdFS) Mkdir(path string, perm fs.FileMode) sys.Errno {
-	return fs.fs(path).Mkdir(path, perm)
-}
-
-func (fs cmdFS) Chmod(path string, perm fs.FileMode) sys.Errno {
-	return fs.fs(path).Chmod(path, perm)
-}
-
-func (fs cmdFS) Rename(from string, to string) sys.Errno {
-	return fs.fs(from).Rename(from, to)
-}
-
-func (fs cmdFS) Rmdir(path string) sys.Errno {
-	return fs.fs(path).Rmdir(path)
-}
-
-func (fs cmdFS) Unlink(path string) sys.Errno {
-	return fs.fs(path).Unlink(path)
-}
-
-func (fs cmdFS) Link(oldPath string, newPath string) sys.Errno {
-	return fs.fs(oldPath).Link(oldPath, newPath)
-}
-
-func (fs cmdFS) Symlink(oldPath string, linkName string) sys.Errno {
-	return fs.fs(oldPath).Symlink(oldPath, linkName)
-}
-
-func (fs cmdFS) Readlink(path string) (string, sys.Errno) {
-	return fs.fs(path).Readlink(path)
-}
-
-func (fs cmdFS) Utimens(path string, atim int64, mtim int64) sys.Errno {
-	return fs.fs(path).Utimens(path, atim, mtim)
-}
-
-func (fs cmdFS) fs(path string) sys.FS {
-	if len(path) > 0 && path[0] != '/' {
-		return fs.cwd
-	}
-	return fs.root
 }
